955558e8e01a80062c3961efc6715cc0d9891cd5
utkarshpandey336-sketch <utkarshpandey336@gmail.com>
2026-07-06 14:52:51 +0530

    Time: 0 ms (100%), Space: 31.2 MB (81.34%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int index=-1;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]==target){
+         index=i;
+        }
+       } 
+       return index;
+    }
+};
