ee1328bacd03cae8ac85697bf2cd78e6773e53f3
utkarshpandey336-sketch <utkarshpandey336@gmail.com>
2026-08-09 12:11:32 +0530

    Time: 1 ms (64.03%), Space: 13.9 MB (94.11%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    void solve(int index, vector<int>& candidates,
-               int target, vector<int>& ds,
-               vector<vector<int>>& ans)
+    void solve(int index, vector<int>& candidates, int target,
+               vector<int>& ds, vector<vector<int>>& ans)
     {
-        // Found a valid combination
-        if(target == 0)
+        if(index == candidates.size())
         {
-            ans.push_back(ds);
-            return;
-        }
-
-        // No more elements or target became negative
-        if(index == candidates.size() || target < 0)
-        {
+            if(target == 0)
+            {
+                ans.push_back(ds);
+            }
             return;
         }
 
         // Pick
-        ds.push_back(candidates[index]);
+        if(candidates[index] <= target)
+        {
+            ds.push_back(candidates[index]);
 
-        // Same index because we can pick the element again
-        solve(index, candidates,
-              target - candidates[index], ds, ans);
+            // Same index because we can pick it again
+            solve(index, candidates, target - candidates[index], ds, ans);
 
-        ds.pop_back();
+            ds.pop_back();
+        }
 
         // Don't Pick
-        solve(index + 1, candidates,
-              target, ds, ans);
+        solve(index + 1, candidates, target, ds, ans);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target)
