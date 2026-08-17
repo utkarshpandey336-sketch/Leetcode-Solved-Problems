7d2f0ffb8c73a22dbf145cddfcad79b1fdbd6462
utkarshpandey336-sketch <utkarshpandey336@gmail.com>
2026-08-18 00:08:41 +0530

    Time: 0 ms (100%), Space: 13.5 MB (26.33%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        // Last row
+        for(int j = 0; j < n; j++) {
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        // Bottom → top
+        for(int i = n-2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++) {
+
+                dp[i][j] = triangle[i][j] +
+                           min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+         return dp[0][0];
+    }
+};
