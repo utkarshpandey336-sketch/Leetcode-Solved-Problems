724af615427dda8f43934091abfcbeacaf4ac6aa
utkarshpandey336-sketch <utkarshpandey336@gmail.com>
2026-08-28 11:33:52 +0530

    Time: 406 ms (42.06%), Space: 211.5 MB (40.29%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,63 +1,32 @@
 class Solution {
 public:
-
-    int solve(int i, int buy, vector<int>& prices,
-              vector<vector<vector<int>>>& dp,
-              int transaction) {
-
-        // No days left
-        if(i == prices.size()) {
-            return 0;
-        }
-
-        // Already completed 2 transactions
-        if(transaction == 2) {
-            return 0;
-        }
-
-        // Already calculated
-        if(dp[i][buy][transaction] != -1) {
-            return dp[i][buy][transaction];
-        }
-
-        if(buy == 1) {
-
-            // Buy OR don't buy
-            return dp[i][buy][transaction] = max(
-                -prices[i] + solve(
-                    i + 1, 0, prices, dp, transaction
-                ),
-
-                solve(
-                    i + 1, 1, prices, dp, transaction
-                )
-            );
-        }
-
-        else {
-
-            // Sell OR don't sell
-            return dp[i][buy][transaction] = max(
-                prices[i] + solve(
-                    i + 1, 1, prices, dp, transaction + 1
-                ),
-
-                solve(
-                    i + 1, 0, prices, dp, transaction
-                )
-            );
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
 
         int n = prices.size();
 
         vector<vector<vector<int>>> dp(
-            n,
-            vector<vector<int>>(2, vector<int>(3, -1))
+            n + 1,
+            vector<vector<int>>(2, vector<int>(3, 0))
         );
 
-        return solve(0, 1, prices, dp, 0);
+        for(int i = n - 1; i >= 0; i--) {
+
+            for(int transaction = 0; transaction < 2; transaction++) {
+
+                // Can buy
+                dp[i][1][transaction] = max(
+                    -prices[i] + dp[i + 1][0][transaction],
+                    dp[i + 1][1][transaction]
+                );
+
+                // Holding stock → can sell
+                dp[i][0][transaction] = max(
+                    prices[i] + dp[i + 1][1][transaction + 1],
+                    dp[i + 1][0][transaction]
+                );
+            }
+        }
+
+        return dp[0][1][0];
     }
 };
