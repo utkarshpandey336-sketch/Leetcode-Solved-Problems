5d33daea2ca99f0b9fac58206686b993bf2fcaec
utkarshpandey336-sketch <utkarshpandey336@gmail.com>
2026-09-20 01:18:07 +0530

    Time: 19 ms (81.14%), Space: 17.4 MB (87.43%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < n - 3; i++) {
+
+            // Skip duplicate i
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for(int j = i + 1; j < n - 2; j++) {
+
+                // Skip duplicate j
+                if(j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while(left < right) {
+
+                    long long sum = (long long)nums[i] + nums[j]
+                                  + nums[left] + nums[right];
+
+                    if(sum < target) {
+                        left++;
+                    }
+                    else if(sum > target) {
+                        right--;
+                    }
+                    else {
+                        ans.push_back({
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        });
+
+                        left++;
+                        right--;
+
+                        // Skip duplicate left values
+                        while(left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+
+                        // Skip duplicate right values
+                        while(left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
